93453abe26c1f2e2bbbc4ac666f339f1f91a3ae7
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-01-25 13:54:01 -0300

    script para captura dos dados do formulario desenvolvido

---
+++ script.js
@@ -0,0 +1,43 @@
+const formulario = document.querySelector("form")
+
+const nome = document.querySelector(".nome")
+const email = document.querySelector(".email")
+const senha = document.querySelector(".senha")
+const telefone = document.querySelector(".telefone")
+
+function cadastrar(usuario){
+    fetch("http://localhost:8080/cadastrar", {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(usuario)
+    })
+    .then(function(res){
+        console.log(res)
+    })
+    .catch(function(res){
+        console.log(res)
+    })
+}
+
+function limparFormulario(){
+    nome = ""
+    email = ""
+    senha = ""
+    telefone = ""
+}
+
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault()
+    const usuario = {
+        nome,
+        email,
+        senha,
+        telefone
+    }
+    cadastrar(usuario)
+    limparFormulario()
+
+})
