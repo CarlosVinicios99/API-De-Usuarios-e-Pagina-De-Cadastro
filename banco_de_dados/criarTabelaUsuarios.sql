5304b59caabfe2f279890697360509ca9e12bcbc
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-01-28 11:06:32 -0300

    Arquivos adicionados para o github

--- banco_de_dados/criarTabelaUsuarios.sql
+++ banco_de_dados/criarTabelaUsuarios.sql
@@ -2,7 +2,7 @@ CREATE TABLE usuarios(
 	id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(60) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    senha VARCHAR(50) NOT NULL,
+    senha VARCHAR(100) NOT NULL,
     telefone VARCHAR(20) NOT NULL,
 	PRIMARY KEY(id),
     UNIQUE KEY(email)
